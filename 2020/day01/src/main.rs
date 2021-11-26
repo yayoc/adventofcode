342e13cc5aba66ec3518ed51d0210de56c407ec1
yayoc <n.yayoshi+github@gmail.com>
2021-11-26 10:05:46 +0900

    extract util lib

--- 2020/day01/src/main.rs
+++ 2020/day01/src/main.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use util;
 
 fn main() {
-    if let Ok(lines) = read_lines("input.txt") {
+    if let Ok(lines) = util::read_lines("input.txt") {
         let nums: Vec<i32> = lines.map(|l| l.unwrap().parse::<i32>().unwrap()).collect();
 
         for num in &nums {
@@ -29,14 +27,4 @@ fn find_two_sum(target: i32, nums: &Vec<i32>) -> Option<(i32, i32)> {
         }
     }
     None
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
