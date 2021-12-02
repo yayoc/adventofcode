73e72511706e51e0ea302f27b5062cfd1bea407e
yayoc <n.yayoshi+github@gmail.com>
2021-12-02 15:23:53 +0900

    day02 part-two

--- 2021/day02/src/main.rs
+++ 2021/day02/src/main.rs
@@ -8,13 +8,18 @@ fn main() {
 
     let mut x = 0;
     let mut y = 0;
+    let mut aim = 0;
 
     for command in commands {
         let mut iter = command.split_whitespace();
         match iter.next() {
-            Some("forward") => x += iter.next().unwrap().parse::<i32>().unwrap(),
-            Some("up") => y -= iter.next().unwrap().parse::<i32>().unwrap(),
-            Some("down") => y += iter.next().unwrap().parse::<i32>().unwrap(),
+            Some("forward") => {
+                let v = iter.next().unwrap().parse::<i32>().unwrap();
+                x += v;
+                y += v * aim;
+            }
+            Some("up") => aim -= iter.next().unwrap().parse::<i32>().unwrap(),
+            Some("down") => aim += iter.next().unwrap().parse::<i32>().unwrap(),
             _ => {}
         }
     }
