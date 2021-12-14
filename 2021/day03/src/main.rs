fca561288fbea2188ff376d9a3d50e2cdb697b1e
yayoc <n.yayoshi+github@gmail.com>
2021-12-14 13:30:49 +0900

    day03 part-two

--- 2021/day03/src/main.rs
+++ 2021/day03/src/main.rs
@@ -1,45 +1,65 @@
-use std::collections::HashMap;
 use util;
+
+fn count_nums(nums: &Vec<String>, pos: usize) -> (i32, i32) {
+    let mut z_cnt = 0;
+    let mut o_cnt = 0;
+
+    for num in nums {
+        let bit = num.chars().nth(pos).unwrap();
+        if bit == '0' {
+            z_cnt += 1;
+        } else {
+            o_cnt += 1;
+        }
+    }
+    (z_cnt, o_cnt)
+}
+
+fn find_common_char(pos: usize, nums: &Vec<String>) -> char {
+    let (z_cnt, o_cnt) = count_nums(nums, pos);
+    if z_cnt > o_cnt {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn find_least_char(pos: usize, nums: &Vec<String>) -> char {
+    let (z_cnt, o_cnt) = count_nums(nums, pos);
+    if o_cnt < z_cnt {
+        '1'
+    } else {
+        '0'
+    }
+}
+
+fn filter_nums(nums: Vec<String>, pos: usize, bit: char) -> Vec<String> {
+    nums.into_iter()
+        .filter(|x| x.chars().nth(pos).unwrap() == bit)
+        .collect()
+}
+
+fn rating(nums: &Vec<String>, f: fn(usize, &Vec<String>) -> char) -> String {
+    let mut pos = 0;
+    let mut res = nums.clone();
+    while res.len() != 1 {
+        let c = f(pos, &res);
+        res = filter_nums(res, pos, c);
+        pos += 1;
+    }
+    res[0].clone()
+}
 
 fn main() {
     let nums: Vec<String> = util::parse_input("input.txt").expect("can't parse input");
-    let len = nums[0].len();
-    let mut map = HashMap::new();
-    map.insert("0", 0);
-    map.insert("1", 0);
-    let mut counter = vec![map; len];
+    let o_rat = rating(&nums, find_common_char);
+    let co2_rat = rating(&nums, find_least_char);
 
-    for num in nums {
-        for (i, c) in num.chars().enumerate() {
-            let count = counter.get_mut(i).expect("");
-            match c {
-                '0' => *count.get_mut("0").unwrap() += 1,
-                '1' => *count.get_mut("1").unwrap() += 1,
-                _ => {}
-            }
-        }
-    }
-
-    let mut gamma = String::from("");
-    for c in counter {
-        if c["0"] > c["1"] {
-            gamma.push_str("0")
-        } else {
-            gamma.push_str("1")
-        }
-    }
-
-    let mut epsilon = String::from("");
-    for c in gamma.chars() {
-        if c == '0' {
-            epsilon.push_str("1")
-        } else {
-            epsilon.push_str("0")
-        }
-    }
+    println!("{}", o_rat);
+    println!("{}", co2_rat);
 
     let res =
-        isize::from_str_radix(&gamma, 2).unwrap() * isize::from_str_radix(&epsilon, 2).unwrap();
+        isize::from_str_radix(&o_rat, 2).unwrap() * isize::from_str_radix(&co2_rat, 2).unwrap();
 
     println!("{:?}", res);
 }
