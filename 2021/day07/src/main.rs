10bcc2bca6fbd8c213e77ec16fc86139bbce9987
yayoc <n.yayoshi+github@gmail.com>
2021-12-17 14:53:13 +0900

    day07 part two

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -1,16 +1,44 @@
 use util;
 
+fn step_cost(mut num: u128) -> u128 {
+    let mut cost = 0;
+    while num > 0 {
+        cost += num;
+        num -= 1;
+    }
+    cost
+}
+
+fn abs_diff(j: u128, k: u128) -> u128 {
+    if j < k {
+        k - j
+    } else {
+        j - k
+    }
+}
+
+fn fuel_cost(positions: &Vec<u128>, pos: u128) -> u128 {
+    positions.iter().map(|p| step_cost(abs_diff(pos, *p))).sum()
+}
+
 fn main() {
     let input: Vec<String> = util::parse_input("input.txt").expect("can't parse input");
-    let mut positions: Vec<i32> = input[0]
+    let mut positions: Vec<u128> = input[0]
         .split(",")
-        .map(|v| v.parse::<i32>().unwrap())
+        .map(|v| v.parse::<u128>().unwrap())
         .collect();
     positions.sort();
-    println!("{:?}", positions);
-    let mid = positions[(positions.len() / 2) as usize];
-    println!("{:?}", mid);
+    println!("positions: {:?}", positions);
+    let last = positions[positions.len() - 1];
+    println!("last: {:?}", last);
+    let mut min = u128::max_value();
 
-    let sum: i32 = positions.iter().map(|v| (v - mid).abs()).sum();
-    println!("{}", sum);
+    for i in 0..=last {
+        let cost = fuel_cost(&positions, i);
+        if cost < min {
+            min = cost;
+        }
+    }
+
+    println!("{}", min);
 }
