9bea87336f04e68440579fc442f8d0f8f5977f64
yayoc <n.yayoshi+github@gmail.com>
2021-12-24 01:09:05 +0900

    day09 part two

--- 2021/day09/src/main.rs
+++ 2021/day09/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         }
     }
 
-    let mut risk_levels = 0;
+    let mut size = vec![];
     let mut y = 1;
     while y <= rows_len {
         let mut x = 1;
@@ -31,11 +31,37 @@ fn main() {
                 .unwrap();
 
             if v.unwrap() < min {
-                risk_levels += v.unwrap() + 1;
+                fn explore(
+                    map: &Vec<Vec<Option<u32>>>,
+                    x: usize,
+                    y: usize,
+                    visited: &mut Vec<(usize, usize)>,
+                ) -> u32 {
+                    if visited.contains(&(x, y)) {
+                        return 0;
+                    }
+
+                    let val = map[y][x];
+                    match val {
+                        Some(9) => return 0,
+                        None => return 0,
+                        _ => {}
+                    }
+                    visited.push((x, y));
+
+                    let top_count = explore(map, x, y - 1, visited);
+                    let right_count = explore(map, x + 1, y, visited);
+                    let bottom_count = explore(map, x, y + 1, visited);
+                    let left_count = explore(map, x - 1, y, visited);
+                    return 1 + top_count + right_count + bottom_count + left_count;
+                }
+                let visited: &mut Vec<(usize, usize)> = &mut vec![];
+                size.push(explore(&heightmap, x, y, visited));
             }
             x += 1;
         }
         y += 1;
     }
-    println!("{}", risk_levels);
+    size.sort_by(|a, b| b.cmp(a));
+    println!("{}", size[0] * size[1] * size[2]);
 }
