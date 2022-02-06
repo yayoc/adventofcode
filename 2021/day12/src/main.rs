062cd7a64179faaa39ac29696b646d31c799a415
yayoc <n.yayoshi+github@gmail.com>
2022-02-06 17:14:31 +0900

    day12 part one

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -1,33 +1,108 @@
-use std::collections::HashMap;
-use util;
+use std::{collections::HashMap, fs};
 
-fn bfs(graph: HashMap<&str, Vec<&str>>, start: &str, paths: &mut Vec<Vec<&str>>) {
-    let mut queue = vec![];
+type Path<'a> = Vec<&'a str>;
 
-    while queue.len() > 0 {
-        let node = queue.pop();
-        if node == "end" {
-            break;
+fn is_lowercase(s: &str) -> bool {
+    s.chars().all(|c| c.is_ascii_lowercase())
+}
+
+fn get_graph(input: &str) -> HashMap<&str, Vec<&str>> {
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let mut nodes = line.split('-');
+        let l = nodes.next().unwrap();
+        let r = nodes.next().unwrap();
+        graph.entry(l).or_insert_with(Vec::new).push(r);
+        graph.entry(r).or_insert_with(Vec::new).push(l);
+    }
+    graph
+}
+
+fn count_paths<'a>(
+    graph: HashMap<&str, Path<'a>>,
+    start: &'a str,
+    end: &'a str,
+    visited: &mut Vec<&'a str>,
+    count: &mut usize,
+) {
+    visited.push(start);
+
+    if start == end {
+        *count += 1;
+    } else {
+        for adj in graph.get(start).unwrap() {
+            if is_lowercase(adj) {
+                if !visited.contains(adj) {
+                    count_paths(graph.clone(), adj, end, visited, count);
+                }
+            } else {
+                count_paths(graph.clone(), adj, end, visited, count);
+            }
         }
     }
+    visited.retain(|x| *x != start);
 }
 
 fn main() {
-    let input: Vec<String> = util::parse_input("input.txt").expect("can't parse input");
-    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
-    for path in &input {
-        let mut nodes = path.split("-");
-        let l = nodes.nth(0).unwrap();
-        let r = nodes.nth(0).unwrap();
-        graph.entry(l).or_insert_with(|| vec![]).push(r);
-        graph.entry(r).or_insert_with(|| vec![]).push(l);
-    }
+    let content: String = fs::read_to_string("input.txt").expect("can't read file");
+    let graph: HashMap<&str, Vec<&str>> = get_graph(content.as_str());
+    let mut visited = vec![];
+    let mut count = 0;
+    count_paths(graph, "start", "end", &mut visited, &mut count);
+    println!("{}", count);
+}
 
-    let mut paths = vec![];
-    for n in graph.get("start") {
-        bfs(graph, n, &mut paths);
-    }
+pub fn solve1(input: &str) -> usize {
+    let graph: HashMap<&str, Vec<&str>> = get_graph(input);
+    let mut visited = vec![];
+    let mut count = 0;
+    count_paths(graph, "start", "end", &mut visited, &mut count);
+    count
+}
 
-    println!("{:?}", paths);
-    println!("{:?}", paths.len());
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let test1 = "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+        assert_eq!(super::solve1(test1), 10);
+
+        let test2 = "dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+        assert_eq!(super::solve1(test2), 19);
+
+        let test3 = "fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+        assert_eq!(super::solve1(test3), 226);
+    }
 }
